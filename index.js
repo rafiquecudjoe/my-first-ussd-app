936b19c83eeffdf32690003461fb3204f7a9d566
Rafique <rasgalazy5@gmail.com>
2021-06-17 10:26:31 +0000

    new update

--- index.js
+++ index.js
@@ -63,4 +63,4 @@ server.post("/*", (req, res) => {
   }
 });
 
-server.listen(port, () => console.log(`Server is running on port ${port}`));
+server.listen(process.env.PORT||5000, () => console.log(`Server is running on port ${port}`));
